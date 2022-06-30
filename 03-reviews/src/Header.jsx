753a140f803a888a9484751aa02a4794b3b35e14
Błażej Domagała <petitoff@outlook.com>
2022-06-30 20:25:11 +0200

    03-reviews

---
+++ 03-reviews/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <main>
+      <div className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Header;
