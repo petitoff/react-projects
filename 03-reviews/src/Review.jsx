0e37379505dcd58181ebabdd356e807ed47bd724
Błażej Domagała <petitoff@outlook.com>
2022-07-01 14:53:24 +0200

    The function randomizes the index of the array

--- 03-reviews/src/Review.jsx
+++ 03-reviews/src/Review.jsx
@@ -30,6 +30,16 @@ const Review = () => {
     });
   };
 
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <article className="review">
       <div className="img-container">
