2957c674dfdf75433d38e53790e59d7a06de60af
Błażej Domagała <petitoff@outlook.com>
2022-07-01 14:47:42 +0200

    Check if we do not go beyond the index of the array

--- 03-reviews/src/Review.jsx
+++ 03-reviews/src/Review.jsx
@@ -6,6 +6,18 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+
+    if (number < 0) {
+      return people.length - 1;
+    }
+
+    return number;
+  };
+
   return (
     <article className="review">
       <div className="img-container">
